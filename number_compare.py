ad18f07a2294b75803cb8eba801916e37154a255
Tom Ghislain <92228660+Flambynou@users.noreply.github.com>
2024-12-16 08:20:01 +0100

    DAG shortest path version

--- number_compare.py
+++ number_compare.py
@@ -7,79 +7,15 @@ class Pathfinder:
 	def __init__(self, reference_number, input_number):
 		self.reference = reference_number
 		self.input = input_number
-		#self.matrix = self.create_matrix()
 		self.matrix = []
-		#self.neighbors = self.precompute_all_neighbors()
 		self.neighbors = {}
-		#self.dictionary = {(0,0):([(0,0)],False, False)}
 		self.dictionary = {}
-		#self.node_paths = self.find_node_paths()
 		self.node_dict = self.create_node_dict()
 		self.node_dict = self.find_node_paths_from_dict()
-		#self.best_node_path = self.get_best_node_path()
 		self.best_node_path = self.get_best_path_from_dict()
-	def create_matrix(self):
-		# Create a matrix of size len(b) x len(a) where each element shows if the digit is the same of not
-		matrix =[]
-		for i in range(len(self.input)):  # loop over every digit in b
-			line = []
-			for j in range(len(self.reference)): # loop over evey digit in a
-				value = not bool(abs(int(self.reference[j])-int(self.input[i])))
-				line.append(value) #appends wether the digit is the same or not as a bool
-			matrix.append(line)
-		return matrix
-	def precompute_all_neighbors(self):
-		neighbors = {}
-		for i in range(len(self.matrix)):
-			for j in range(len(self.matrix[0])):
-				if self.matrix[i][j] or (i==0 and j==0):
-					neighbor_list = []
-					for k in range(i+1,len(self.matrix)):
-						for l in range(j+1,len(self.matrix[0])):
-							if self.matrix[k][l]:
-								neighbor_list.append((k,l))
-					neighbors[(i,j)] = neighbor_list
-		neighbors[(-1,-1)] = [(i,j) for i in range(len(self.matrix)) for j in range(len(self.matrix[0])) if self.matrix[i][j]]
-		return neighbors
-	def find_node_paths(self):
-		while True:
-			tmp_dict = self.dictionary.copy()
-			for node in tmp_dict:
-				if not self.dictionary[node][1]:
-					for neighbor in self.neighbors.get(node,[]):
-						if neighbor not in self.dictionary or get_value(self.dictionary[node][0]+[neighbor]) < get_value(self.dictionary[neighbor][0]):
-							self.dictionary[neighbor] = (self.dictionary[node][0]+[neighbor],False, bool(self.neighbors.get(neighbor,[])))
-					self.dictionary[node] = (self.dictionary[node][0],True, bool(self.neighbors.get(node,[])))
-			if not False in [self.dictionary[node][1] for node in self.dictionary]:
-				break
-	def get_best_node_path(self):
-		best_node = ([],float("inf"))
-		for node in self.dictionary:
-			if self.dictionary[node][1] and self.neighbors.get(node, []) == []:
-				if get_value(self.dictionary[node][0]) < best_node[1]:
-					best_node = (self.dictionary[node][0],get_value(self.dictionary[node][0]))
-		return best_node[0]
-
-	def __str__(self):
-		# The str method returns a string with the matrix, the node dictionary and the best node path
-		string = "Matrix : \n"
-		for column in range(len(self.matrix[0])):
-			for row in self.matrix:
-				if row[column]:
-					string += "█"
-				else:
-					string += "░"
-			string+="\n"
-		string += "Node dictionary : \n"
-		for node in self.dictionary:
-			string += f"Node {node} : " + str(self.dictionary[node][0]) + " Value: " + str(get_value(self.dictionary[node][0])) + " " + str(self.dictionary[node][1]) + "\n"
-		string += "Best node path : " + str(self.best_node_path)
-		return string
-
-	#Test of optimized version by not creating a matrix, instead creating a dictionary with each true node and no neighbor list but simply iterate over the dictionary
 
 	def create_node_dict(self):
-		node_dict = {(0,0):([(0,0)],True)}
+		node_dict = {(-1,-1):([],True)}
 		for i in range(len(self.input)):
 			for j in range(len(self.reference)):
 				if not bool(abs(int(self.reference[j])-int(self.input[i]))):
@@ -100,10 +36,64 @@ class Pathfinder:
 	def get_best_path_from_dict(self):
 		best_node = ([],float("inf"))
 		for node in self.node_dict:
-			if self.node_dict[node][1]:
+			if node != (-1,-1):
 				if get_value(self.node_dict[node][0]) < best_node[1]:
 					best_node = (self.node_dict[node][0],get_value(self.node_dict[node][0]))
 		return best_node[0]
+
+	# Test of shortest path in a DAG
+
+
+
+def create_sorted_list(input,reference):
+	node_list = []
+	for row in range(len(input)):
+		for element in range(len(reference)):
+			if reference[element] == input[row]:
+				node_list.append((row,element))
+	return node_list
+
+
+def create_adjacency_index_list(node_list):
+	adjacency_index_list = [[] for node in node_list]
+	for node_index in range(len(node_list)):
+		x,y = node_list[node_index]
+		for neighbor_index in range(node_index+1,len(node_list)):
+			if node_list[neighbor_index][0] > x and node_list[neighbor_index][1] > y:
+				adjacency_index_list[node_index].append(neighbor_index)
+	return node_list, adjacency_index_list
+
+
+def DAG_path(node_list, adjacency_index_list):
+	dist = [float("inf") for _ in range(len(node_list))]
+	dist[0] = 0
+	predecessor_index = [-1 for node in node_list]
+	best_index = -1
+	best_dist = float("inf")
+	for node_index in range(len(node_list)):
+		for neighbor_index in adjacency_index_list[node_index]:
+			alt = dist[node_index] + find_edge_weight(node_list[node_index],node_list[neighbor_index])
+			if  alt < dist[neighbor_index]:
+				dist[neighbor_index] = alt
+				predecessor_index[neighbor_index] = node_index
+		if dist[node_index] < best_dist:
+			best_dist = dist[node_index]
+			best_index = node_index
+	return dist, predecessor_index, best_index
+
+
+def DAG_shortest_path(node_list, dist, predecessor_index, best_index):
+	best_path = [node_list[best_index]]
+	predecessor = predecessor_index[best_index]
+	while predecessor >= 0:
+		best_path.append(node_list[predecessor])
+		predecessor = predecessor_index[predecessor]
+	return best_path
+
+
+EDGE_WEIGHT_BIAS = 3
+def find_edge_weight(smallernode ,greaternode):
+	return abs(greaternode[0] - smallernode[0]) + abs(greaternode[1] - smallernode[0]) - EDGE_WEIGHT_BIAS
 
 
 
@@ -162,9 +152,25 @@ def get_right_list(decoded_path):
 	return right_list
 # Test the functions
 
-m = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989"
-t = "14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460"
+m = "14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912"
+t = "14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912"
 
 
-print(decode_path(fill_path_holes(Pathfinder(m,t).best_node_path,m,t),m,t))
+#print(decode_path(fill_path_holes(Pathfinder(m,t).best_node_path,m,t),m,t))
+
+for _ in range(20):
+	print()
+
+# DAG test
+
+thenodelist = create_sorted_list(m,t)
+theadjacencylist = create_adjacency_index_list(thenodelist)
+thedist, thepredecessorindex, thebestindex = DAG_path(*theadjacencylist)
+thepath = DAG_shortest_path(thenodelist, thedist, thepredecessorindex, thebestindex)
+
+print(f"Path : {thepath}")
+
+# Test the class version
+
+#print(f"Path : {Pathfinder(m,t).best_node_path}")
 
